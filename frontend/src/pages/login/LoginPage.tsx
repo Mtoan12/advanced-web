ced850cf5fbf667976cc294d89d1523b63a417da
mtoan12 <20120215@student.hcmus.edu.vn>
2023-11-14 12:09:08 +0700

    border đỏ input error

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -8,6 +8,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import { userSchema } from "@/schema/formSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -28,7 +29,7 @@ const LoginPage = () => {
   };
   return (
     <main className="container flex max-w-[1024px] flex-col justify-center py-20">
-      <h1 className="text-center text-3xl font-bold">Login</h1>
+      <h1 className="mb-4 text-center text-3xl font-bold">Login</h1>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
@@ -38,7 +39,14 @@ const LoginPage = () => {
               <FormItem>
                 <FormLabel>Username:</FormLabel>
                 <FormControl>
-                  <Input placeholder="Username" {...field} />
+                  <Input
+                    placeholder="Username"
+                    {...field}
+                    className={cn(
+                      form.formState.errors.username &&
+                        "border-red-400 focus-visible:ring-red-400",
+                    )}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -51,7 +59,15 @@ const LoginPage = () => {
               <FormItem>
                 <FormLabel>Password:</FormLabel>
                 <FormControl>
-                  <Input type="password" placeholder="Password" {...field} />
+                  <Input
+                    type="password"
+                    placeholder="Password"
+                    {...field}
+                    className={cn(
+                      form.formState.errors.password &&
+                        "border-red-400 focus-visible:ring-red-400",
+                    )}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
