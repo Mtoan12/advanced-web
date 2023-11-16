004f30a62c734faa534eb3663f96be740a985203
duytruong-23 <zduytruongz@gmail.com>
2023-11-16 16:05:04 +0700

    add spring security and create entities such as user, role.

---
+++ backend/src/main/java/com/example/backend/entities/Role.java
@@ -0,0 +1,32 @@
+package com.example.backend.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "role_name")
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    @JsonIgnore
+    List<User> users;
+
+
+}
---
+++ backend/src/main/java/com/example/backend/entities/User.java
@@ -0,0 +1,83 @@
+package com.example.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+     private String email;
+
+    @Column(name = "password", nullable = false)
+     private String password;
+
+     @Column(name = "first_name")
+     private String firstName;
+
+     @Column(name = "last_name")
+     private String lastName;
+
+     @Column(name = "gender")
+     private Boolean gender;
+
+     @Column(name = "is_online")
+     private Boolean isOnline;
+
+     @Column(name = "date_of_birth")
+     private Date DOB;
+
+     @ManyToOne(fetch = FetchType.LAZY)
+     @JoinColumn(name = "role_id")
+     private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
