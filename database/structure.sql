1bfcaedd769e2e2114e107c693093a807cc1db85
duytruong-23 <zduytruongz@gmail.com>
2023-11-17 11:15:29 +0700

    edit database

--- database/structure.sql
+++ database/structure.sql
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	user_id BIGINT AUTO_INCREMENT NOT NULL,
     role_id BIGINT NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
