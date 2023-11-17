284e37ba16bd6e430593d86452935d4177dfa4e6
duytruong-23 <zduytruongz@gmail.com>
2023-11-17 11:13:35 +0700

    add security config

--- backend/src/main/java/com/example/backend/configurations/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/configurations/SecurityConfig.java
@@ -1,9 +1,53 @@
 package com.example.backend.configurations;
 
+import com.example.backend.components.JWTFilter;
+import com.example.backend.constants.AppConstant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.util.ArrayList;
+import java.util.List;
 
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JWTFilter jwtFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTAuthenticationEntryPoint unauthorizedHandler;
 
+@Bean
+    public List<RequestMatcher> permitAllRequestMatchers() {
+    List<RequestMatcher> permitAllMatchers = new ArrayList<>();
+
+    permitAllMatchers.add(new AntPathRequestMatcher(AppConstant.AUTHENTICATION_PATH));
+
+    return permitAllMatchers;
+}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(unauthorizedHandler))
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(permitAllRequestMatchers().toArray(new RequestMatcher[0])).permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 
 }
