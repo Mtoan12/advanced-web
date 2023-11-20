fb4072b9488db829d7cbbe9d9d0fc75c9c6a3cea
duytruong-23 <zduytruongz@gmail.com>
2023-11-20 10:30:41 +0700

    add cors method allow

--- backend/src/main/java/com/example/backend/configurations/CORSConfig.java
+++ backend/src/main/java/com/example/backend/configurations/CORSConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 }
